efdab4a737b455f78b1a9f9d3f9d550a074f4310
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-12-27 18:29:41 +0900

    座席にscreen_idを追加する

--- db/migrate/20241227073341_create_screens.rb
+++ db/migrate/20241227073341_create_screens.rb
@@ -1,8 +1,11 @@
 class CreateScreens < ActiveRecord::Migration[7.2]
   def change
-    create_table :screens do |t|
-      t.integer :capacity, null: false
-      t.timestamps
+    create_table "seats", force: :cascade do |t|
+      t.bigint "screen_id", null: false
+      t.string "seat_number"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
     end
+
   end
 end
